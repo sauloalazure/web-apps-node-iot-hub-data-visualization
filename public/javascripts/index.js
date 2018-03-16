384e687c21ab4266c662cb72f95f55261e8beaa3
Saulo Aflitos <sauloal@gmail.com>
2018-03-16 22:04:17 +0100

    error message on websocket disconnect

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,7 +11,10 @@ $(document).ready(function () {
     loadSensors(json);
     
     var ws = new WebSocket("wss://" + location.host);
-      ws.onopen = function () {
+    
+    ws.onclose = onSocketError; 
+    
+    ws.onopen = function () {
       console.log("Successfully connect WebSocket");
     };
     
@@ -32,7 +35,7 @@ $(document).ready(function () {
     console.error( "Request Failed: " + err );
   });
 });
-  
+
 function loadSensors(config) {
   var sensors = config.sensors;
   var measurements = config.measurements;
@@ -394,22 +397,42 @@ function parseTimeStamp(ts) {
   return nd;
 }
 
-function isJson(item) {
-  //https://stackoverflow.com/questions/9804777/how-to-test-if-a-string-is-json-or-not
-  item = typeof item !== "string" ? JSON.stringify(item) : item;
+function onSocketError(event) {
+  // https://stackoverflow.com/questions/18803971/websocket-onerror-how-to-read-error-description
+  var reason;
+  alert("websocket closed", event.code);
+  // See http://tools.ietf.org/html/rfc6455#section-7.4.1
+  if (event.code == 1000)
+      reason = "Normal closure, meaning that the purpose for which the connection was established has been fulfilled.";
+  else if(event.code == 1001)
+      reason = "An endpoint is \"going away\", such as a server going down or a browser having navigated away from a page.";
+  else if(event.code == 1002)
+      reason = "An endpoint is terminating the connection due to a protocol error";
+  else if(event.code == 1003)
+      reason = "An endpoint is terminating the connection because it has received a type of data it cannot accept (e.g., an endpoint that understands only text data MAY send this if it receives a binary message).";
+  else if(event.code == 1004)
+      reason = "Reserved. The specific meaning might be defined in the future.";
+  else if(event.code == 1005)
+      reason = "No status code was actually present.";
+  else if(event.code == 1006)
+     reason = "The connection was closed abnormally, e.g., without sending or receiving a Close control frame";
+  else if(event.code == 1007)
+      reason = "An endpoint is terminating the connection because it has received data within a message that was not consistent with the type of the message (e.g., non-UTF-8 [http://tools.ietf.org/html/rfc3629] data within a text message).";
+  else if(event.code == 1008)
+      reason = "An endpoint is terminating the connection because it has received a message that \"violates its policy\". This reason is given either if there is no other sutible reason, or if there is a need to hide specific details about the policy.";
+  else if(event.code == 1009)
+     reason = "An endpoint is terminating the connection because it has received a message that is too big for it to process.";
+  else if(event.code == 1010) // Note that this status code is not used by the server, because it can fail the WebSocket handshake instead.
+      reason = "An endpoint (client) is terminating the connection because it has expected the server to negotiate one or more extension, but the server didn't return them in the response message of the WebSocket handshake. <br /> Specifically, the extensions that are needed are: " + event.reason;
+  else if(event.code == 1011)
+      reason = "A server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.";
+  else if(event.code == 1015)
+      reason = "The connection was closed due to a failure to perform a TLS handshake (e.g., the server certificate can't be verified).";
+  else
+      reason = "Unknown reason";
 
-  try {
-    item = JSON.parse(item);
-  } catch (e) {
-    return [ false, item ];
-  }
-  
-  if (typeof item === "object" && item !== null) {
-    return [ true, item ];
-  }
-  
-  return [ false, item ];
-}
+  $("body").html("<h1>" + "The connection was closed for reason:</h1><br/><h3>" + reason + "<h3>");
+};
 
 function hexToRGB(hex) {
   //http://www.javascripter.net/faq/hextorgb.htm
@@ -427,3 +450,19 @@ function hexToG(h) { return parseInt((cutHex(h)).substring(2,4),16); }
 function hexToB(h) { return parseInt((cutHex(h)).substring(4,6),16); }
 function cutHex(h) { return (h.charAt(0)=="#") ? h.substring(1,7):h; }
 
+function isJson(item) {
+  //https://stackoverflow.com/questions/9804777/how-to-test-if-a-string-is-json-or-not
+  item = typeof item !== "string" ? JSON.stringify(item) : item;
+
+  try {
+    item = JSON.parse(item);
+  } catch (e) {
+    return [ false, item ];
+  }
+  
+  if (typeof item === "object" && item !== null) {
+    return [ true, item ];
+  }
+  
+  return [ false, item ];
+}
